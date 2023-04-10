292b5aa7b4a8d03b3e9313218315b279ccbe8606
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-04-09 22:12:34 -0400

    Completed the Lab and Task 5, Temperature Converter, declaring a fahrenheit temperature int and then declaring a double variable for cel assigned to the euqation needed to convert the fahrenheit temp into a celsius temp.

--- Variables-Lab/Program.cs
+++ Variables-Lab/Program.cs
@@ -16,7 +16,11 @@ class Program
         //Console.WriteLine("Hello, "+firstName+" "+lastName+"!");
         string fullName = firstName + " " + lastName;
         Console.WriteLine("My first name is "+firstName+" and my last name is "+lastName+", which means my full name is "+fullName+".");
-        
+
+
+        int fer = 100;
+        double cel = (double)(fer - 32) * 5 / 9;
+        Console.WriteLine("Celsius temperature: "+cel+" degrees celsius.");
     }
 }
 
