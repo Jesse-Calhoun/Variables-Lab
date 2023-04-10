506da4dcb72656791d63ec9b344e351e4ef0d88a
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-04-09 21:45:45 -0400

    Finshed Task one by declaring a variable to store my age and used string interpolation to write out a message + Task 2 I delcared a varaible called firstName that was assigned to a Console.ReadLine function which gets the user to input in the terminal + Added a Console.WriteLine to ask the user to enter their age and then another string interpolation tying the user input into the message written, being 'Hello, Jesse!'.

--- Variables-Lab/Program.cs
+++ Variables-Lab/Program.cs
@@ -3,7 +3,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Help, World!");
+        Console.WriteLine("Hello, World!");
+
+        int age = 28;
+
+        Console.WriteLine("I am "+age+" years old");
+
+        Console.WriteLine("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Hello, "+firstName+"!");
+        
     }
 }
 
